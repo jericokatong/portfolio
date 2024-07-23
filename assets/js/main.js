0107b4753a86615fc4864bfbac956af1fc698349
jericokatong <jericokatong@gmail.com>
2024-07-23 16:06:03 +0800

    update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,77 @@
 // Menu Show Y Hidden
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 // Menu Show
 // if constant exists
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
   });
 }
 
 // Hidden Menu
 if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
   });
 }
 
 // REMOVE MENU MOBILE
-const navLink = document.querySelectorAll(".nav_link");
+const navLink = document.querySelectorAll('.nav_link');
 
 navLink.forEach((n) =>
-  n.addEventListener("click", () => {
-    const navMenu = document.getElementById("nav-menu");
-    navMenu.classList.remove("show-menu");
+  n.addEventListener('click', () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
   })
 );
 
 // SKILLS ARROW
-const skillsContent = document.getElementsByClassName("skills_content"),
-  skillsHeader = document.querySelectorAll(".skills_header");
+const skillsContent = document.getElementsByClassName('skills_content'),
+  skillsHeader = document.querySelectorAll('.skills_header');
 
 function toggleSkills() {
   let itemClass = this.parentNode.className;
 
   for (let i = 0; i < skillsContent.length; i++) {
-    skillsContent[i].className = "skills_content skills_close";
+    skillsContent[i].className = 'skills_content skills_close';
   }
   console.log(itemClass);
-  if (itemClass === "skills_content skills_close") {
-    this.parentNode.className = "skills_content skills_open";
+  if (itemClass === 'skills_content skills_close') {
+    this.parentNode.className = 'skills_content skills_open';
   }
 }
 
 skillsHeader.forEach((el) => {
-  el.addEventListener("click", toggleSkills);
+  el.addEventListener('click', toggleSkills);
 });
 
 // QUALIFICATION_TABS
-const tabs = document.querySelectorAll("[data-target]"),
-  tabContents = document.querySelectorAll("[data-content]");
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]');
 
 tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
+  tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.target);
 
     tabContents.forEach((tabContent) => {
-      tabContent.classList.remove("qualification_active");
+      tabContent.classList.remove('qualification_active');
     });
 
-    target.classList.add("qualification_active");
+    target.classList.add('qualification_active');
 
     tabs.forEach((tab) => {
-      tab.classList.remove("qualification_active");
+      tab.classList.remove('qualification_active');
     });
 
-    tab.classList.add("qualification_active");
+    tab.classList.add('qualification_active');
   });
 });
 
 //   SCROLL SECTIONS ACTIVE LINK
-const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
   const scrollY = window.pageYOffset;
@@ -79,112 +79,134 @@ function scrollActive() {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    sectionId = current.getAttribute('id');
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector(".nav_menu a[href*=" + sectionId + "]").classList.add("active-link");
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
     } else {
-      document.querySelector(".nav_menu a[href*=" + sectionId + "]").classList.remove("active-link");
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
     }
   });
 }
-window.addEventListener("scroll", scrollActive);
+window.addEventListener('scroll', scrollActive);
 
 // typing effect
 let i = 0;
-const text = "Backend Developer";
-let blankText = "";
+let wordIndex = 0;
+const words = [
+  'Web Developer',
+  'Frontend Developer',
+  'Backend Developer',
+  'NodeJS Developer',
+];
+let currentWord = words[wordIndex];
+let blankText = '';
 
 function typing() {
-  blankText = text.slice(0, ++i);
-  document.querySelector(".home_data .home_subtitle").textContent = blankText;
+  blankText = currentWord.slice(0, ++i);
+  document.querySelector('.home_data .home_subtitle').textContent = blankText;
 
-  if (blankText.length == text.length) {
+  if (blankText.length == currentWord.length) {
     i = 0;
+    wordIndex = (wordIndex + 1) % words.length;
+    currentWord = words[wordIndex];
+    setTimeout(typing, 2000); // Jeda 2 detik sebelum mengetik kata berikutnya
+  } else {
+    setTimeout(typing, 300);
   }
-  setTimeout(typing, 300);
 }
 
 typing();
 
 // CHANGE BACKGROUND HEADER
 function scrollHeader() {
-  const nav = document.getElementById("header");
+  const nav = document.getElementById('header');
   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 200) nav.classList.add("scroll-header");
-  else nav.classList.remove("scroll-header");
+  if (this.scrollY >= 200) nav.classList.add('scroll-header');
+  else nav.classList.remove('scroll-header');
 }
 
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener('scroll', scrollHeader);
 
 // SHOW SCROLL TOP
 function scrollUp() {
-  const scrollUp = document.getElementById("scroll-up");
+  const scrollUp = document.getElementById('scroll-up');
 
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with scroll-top
-  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-  else scrollUp.classList.remove("show-scroll");
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
 }
 
-window.addEventListener("scroll", scrollUp);
+window.addEventListener('scroll', scrollUp);
 
 // =================== DARK LIGHT THEME ====================
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
 // We obtaion the current theme that the interface has by validating the dark-theme
-const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
-const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun");
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](iconTheme);
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
+    iconTheme
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
+themeButton.addEventListener('click', () => {
   // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
 
   // We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
 });
 
 // Contact Form
-const scriptURL = "https://script.google.com/macros/s/AKfycbxDA6RW6OA3vwGksJE1mX_PQIPqTn3cT8TwIqsdDg2JIApNhIL2iWVlriwkEUZ970kX/exec";
-const form = document.forms["jerico-contact-form"];
-const btnKirim = document.querySelector(".btn-kirim");
-const btnLoading = document.querySelector(".btn-loading");
-const alert = document.querySelector(".alert");
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbxDA6RW6OA3vwGksJE1mX_PQIPqTn3cT8TwIqsdDg2JIApNhIL2iWVlriwkEUZ970kX/exec';
+const form = document.forms['jerico-contact-form'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const alert = document.querySelector('.alert');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   // ketika tombol send diklik
   // tampilkan tombol loading, hilangkan tombol kirim
-  btnLoading.classList.toggle("d-none");
-  btnKirim.classList.toggle("d-none");
+  btnLoading.classList.toggle('d-none');
+  btnKirim.classList.toggle('d-none');
 
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then((response) => {
-      console.log("Success!", response);
+      console.log('Success!', response);
 
       // tampilkan tombol kirim, hilangkan tombol loading
-      btnLoading.classList.toggle("d-none");
-      btnKirim.classList.toggle("d-none");
+      btnLoading.classList.toggle('d-none');
+      btnKirim.classList.toggle('d-none');
       //   tampilkan alert
-      alert.classList.toggle("d-none");
+      alert.classList.toggle('d-none');
 
       //   reset form
       form.reset();
     })
-    .catch((error) => console.error("Error!", error.message));
+    .catch((error) => console.error('Error!', error.message));
 });
